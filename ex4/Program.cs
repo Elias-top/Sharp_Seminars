468bc04b2ff3e19e54aaf4b469add6d4c8037062
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-06-11 23:08:58 +0300

    Сделал 4 задачу первого семинара

---
+++ ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+int first_num = 0;
+int second_num = 0;
+int third_num = 0;
+int[] num_array = new int[3];
+
+int max_num = first_num;
+int index = 0;
+
+Console.WriteLine("Введите первое число: ");
+num_array[0] = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите второе число: ");
+num_array[1] = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите третье число: ");
+num_array[2] = int.Parse(Console.ReadLine() ?? "");
+
+while(index < num_array.Length)
+{
+    if(max_num < num_array[index])
+    {
+        max_num = num_array[index];
+    }
+    index ++;
+}
+
+Console.WriteLine($"Максимальное из введеных чисел = {max_num}");
+
