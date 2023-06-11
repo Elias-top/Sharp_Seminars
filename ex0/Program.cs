76afbfb0b612d6193380ec2c59dc887953113bd0
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-06-11 22:26:51 +0300

    0 пример закончен

--- ex0/Program.cs
+++ ex0/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//int num_1 = 0;
+//int num_2 = 0;
+Console.Clear();
+string user_input = "";
+int user_num = 0;
+int result = 0;
+Console.Write("Введите первое число: ");
+user_input = Console.ReadLine() ?? "";
+user_num = int.Parse(user_input);
+result = user_num * user_num;
+Console.WriteLine($"Ваше число {user_num} в квадрате равняется {result}");
