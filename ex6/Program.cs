0ec9491257654db48f59eedc346484dee34363fb
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-06-11 23:15:36 +0300

    сделал 6 задачу первого семинара

---
+++ ex6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int user_num = 0;
+
+Console.WriteLine("Введите число для проверки на четность: ");
+user_num = int.Parse(Console.ReadLine() ?? "");
+
+if((user_num % 2) == 0)
+{
+    Console.WriteLine($"Число {user_num} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {user_num} не является четным");
+}
+
+
