c150389f05a7016459727496df6b26a30118c45e
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-06-11 22:42:27 +0300

    Сделана вторая задача первого семинара

---
+++ ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int first_num = 0;
+int second_num = 0;
+
+Console.WriteLine("Введите первое число: ");
+first_num = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите второе число: ");
+second_num = int.Parse(Console.ReadLine() ?? "");
+
+if(first_num > second_num)
+{
+    Console.WriteLine($"Число a = {first_num} > числа b = {second_num}");
+}
+else if(first_num == second_num)
+{
+    Console.WriteLine($"Число a = {first_num} равно числу b = {second_num}");
+}
+else
+{
+    Console.WriteLine($"Число a = {first_num} < числа b = {second_num}");
+}
